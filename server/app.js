bceb00341a60e5ed0ef12d9c51533b06a98c4408
vidsuharyadiasep <89774451+vidsuharyadiasep@users.noreply.github.com>
2021-10-31 19:34:25 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,6 +7,47 @@ let key = ''
 
 app.use(express.json());
 
+// TESTING 
+app.get('/', (req, res) => {
+    res.send('Hello From Agora On-Premise Recording Server');
+  });
+
+app.post('/recorder/v1/start', (req, res, next) => {
+    let { body } = req;
+    let { appid, channel, key } = body;
+    if (!appid) {
+        throw new Error("appid is mandatory");
+    }
+    if (!channel) {
+        throw new Error("channel is mandatory");
+    }
+
+    RecordManager.start(key, appid, channel).then(recorder => {
+        //start recorder success
+        res.status(200).json({
+            success: true,
+            sid: recorder.sid
+        });
+    }).catch((e) => {
+        //start recorder failed
+        next(e);
+    });
+})
+
+app.post('/recorder/v1/stop', (req, res, next) => {
+    let { body } = req;
+    let { sid } = body;
+    if (!sid) {
+        throw new Error("sid is mandatory");
+    }
+
+    RecordManager.stop(sid);
+    res.status(200).json({
+        success: true
+    });
+})
+
+//Handle Request from Client UI
 app.post('/recorder/v1/start:channel', (req, res, next) => {
     let  { channel } = req.params.channel;
     if (!channel) {
